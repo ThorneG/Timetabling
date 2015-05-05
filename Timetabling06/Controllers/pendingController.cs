a7ab4537cddf4762ef9336644201a914e569b91a
ThorneG <t.gladwin-13@student.lboro.ac.uk>
2015-05-05 15:26:01 +0100

    Update pendingController.cs

--- Timetabling06/Controllers/pendingController.cs
+++ Timetabling06/Controllers/pendingController.cs
@@ -15,7 +15,7 @@ namespace Timetabling06.Controllers
         private team06Entities db = new team06Entities();
 
         // GET: pending
-        public ActionResult Index(string user)
+        public ActionResult Index(string user, string searchString)
         {
             if (user == null)
             {
@@ -23,7 +23,17 @@ namespace Timetabling06.Controllers
             }
             else
             {
-                var requests = db.requests.Include(r => r.department).Include(r => r.module).Include(r => r.round).Where(r => r.deptCode == user).Where(r => r.sent == 1).Where(r => r.status == 0);
+
+                var requests = from r in db.requests
+                               select r;
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    requests = requests.Where(r => r.moduleCode.Contains(searchString)
+                                           || r.module.moduleTitle.Contains(searchString)
+                                           || r.type.Contains(searchString)
+                                           || r.otherReqs.Contains(searchString)
+                                           || r.weeks.Contains(searchString));
+                }
                 ViewBag.user = user;
                 return View(requests.ToList());
             }
@@ -188,6 +198,108 @@ namespace Timetabling06.Controllers
                 db.requests.Remove(request);
                 db.SaveChanges();
                 return RedirectToAction("Index", new { user = user });
+            }
+        }
+
+        // GET: pending/Sort
+        public ActionResult Sort(string user, string pass, string searchString)
+        {
+            if (user == null)
+            {
+                return RedirectToAction("Index", "Home", new { error = "Please Log In." });
+            }
+            else
+            {
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    var requests = from r in db.requests
+                                   select r;
+                    requests = requests.Where(r => r.moduleCode.Contains(searchString)
+                                           || r.module.moduleTitle.Contains(searchString)
+                                           || r.type.Contains(searchString)
+                                           || r.otherReqs.Contains(searchString)
+                                           || r.weeks.Contains(searchString));
+                    ViewBag.user = user;
+                    return View(requests.ToList());
+                }
+                if (pass == "roundid")
+                {
+                    var requests = db.requests.Include(r => r.department).Include(r => r.module).Include(r => r.round).Where(r => r.deptCode == user).Where(r => r.sent == 1).Where(r => r.status == 0).OrderBy(r => r.roundID);
+                    ViewBag.user = user;
+                    return View(requests.ToList());
+                }
+                else if (pass == "part")
+                {
+                    var requests = db.requests.Include(r => r.department).Include(r => r.module).Include(r => r.round).Where(r => r.deptCode == user).Where(r => r.sent == 1).Where(r => r.status == 0).OrderBy(r => r.moduleCode).ThenBy(r => r.day).ThenBy(r => r.start);
+                    ViewBag.user = user;
+                    return View(requests.ToList());
+                }
+                else if (pass == "module")
+                {
+                    var requests = db.requests.Include(r => r.department).Include(r => r.module).Include(r => r.round).Where(r => r.deptCode == user).Where(r => r.sent == 1).Where(r => r.status == 0).OrderBy(r => r.module.moduleTitle).ThenBy(r => r.day).ThenBy(r => r.start);
+                    ViewBag.user = user;
+                    return View(requests.ToList());
+                }
+                else if (pass == "priority")
+                {
+                    var requests = db.requests.Include(r => r.department).Include(r => r.module).Include(r => r.round).Where(r => r.deptCode == user).Where(r => r.sent == 1).Where(r => r.status == 0).OrderByDescending(r => r.priority);
+                    ViewBag.user = user;
+                    return View(requests.ToList());
+                }
+                else if (pass == "day")
+                {
+                    var requests = db.requests.Include(r => r.department).Include(r => r.module).Include(r => r.round).Where(r => r.deptCode == user).Where(r => r.sent == 1).Where(r => r.status == 0).OrderBy(r => r.day);
+                    ViewBag.user = user;
+                    return View(requests.ToList());
+                }
+                else if (pass == "start")
+                {
+                    var requests = db.requests.Include(r => r.department).Include(r => r.module).Include(r => r.round).Where(r => r.deptCode == user).Where(r => r.sent == 1).Where(r => r.status == 0).OrderBy(r => r.start);
+                    ViewBag.user = user;
+                    return View(requests.ToList());
+                }
+                else if (pass == "length")
+                {
+                    var requests = db.requests.Include(r => r.department).Include(r => r.module).Include(r => r.round).Where(r => r.deptCode == user).Where(r => r.sent == 1).Where(r => r.status == 0).OrderBy(r => r.length);
+                    ViewBag.user = user;
+                    return View(requests.ToList());
+                }
+                else if (pass == "weeks")
+                {
+                    var requests = db.requests.Include(r => r.department).Include(r => r.module).Include(r => r.round).Where(r => r.deptCode == user).Where(r => r.sent == 1).Where(r => r.status == 0).OrderBy(r => r.weeks);
+                    ViewBag.user = user;
+                    return View(requests.ToList());
+                }
+                else if (pass == "type")
+                {
+                    var requests = db.requests.Include(r => r.department).Include(r => r.module).Include(r => r.round).Where(r => r.deptCode == user).Where(r => r.sent == 1).Where(r => r.status == 0).OrderByDescending(r => r.type);
+                    ViewBag.user = user;
+                    return View(requests.ToList());
+                }
+                else if (pass == "date")
+                {
+                    var requests = db.requests.Include(r => r.department).Include(r => r.module).Include(r => r.round).Where(r => r.deptCode == user).Where(r => r.sent == 1).Where(r => r.status == 0).OrderBy(r => r.date);
+                    ViewBag.user = user;
+                    return View(requests.ToList());
+                }
+                else if (pass == "other")
+                {
+                    var requests = db.requests.Include(r => r.department).Include(r => r.module).Include(r => r.round).Where(r => r.deptCode == user).Where(r => r.sent == 1).Where(r => r.status == 0).OrderBy(r => r.otherReqs);
+                    ViewBag.user = user;
+                    return View(requests.ToList());
+                }
+                else if (pass == "link")
+                {
+                    var requests = db.requests.Include(r => r.department).Include(r => r.module).Include(r => r.round).Where(r => r.deptCode == user).Where(r => r.sent == 1).Where(r => r.status == 0).OrderBy(r => r.requestLink);
+                    ViewBag.user = user;
+                    return View(requests.ToList());
+                }
+                else
+                {
+                    var requests = db.requests.Include(r => r.department).Include(r => r.module).Include(r => r.round).Where(r => r.deptCode == user).Where(r => r.sent == 1).Where(r => r.status == 0).OrderBy(r => r.status);
+                    ViewBag.user = user;
+                    return View(requests.ToList());
+                }
             }
         }
 
